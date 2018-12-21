93693383e10b07835d37e47f760f360bbb2062f0
Dumilk <dtd66@nau.edu>
2018-12-20 19:09:39 -0700

    convertedRoute now working correctly
    converter route working as intended and commneted

--- udacity_app/lib/converter_route.dart
+++ udacity_app/lib/converter_route.dart
@@ -5,11 +5,20 @@ import 'unit.dart';
 
 const _padding = EdgeInsets.all(16.0);
 
+/// [ConverterRoute] where users can input amounts to convert in one [Unit]
+/// and retrieve the conversion in another [Unit] for a specific [Category].
 class ConverterScreen extends StatefulWidget {
-  final List<Unit> units;
-  final name;
-  final color;
 
+  /// Units for this [Category].
+  final List<Unit> units;
+
+  /// [Category]'s name
+  final String name;
+
+  /// Color for the [Category]
+  final Color color;
+
+  /// this [ConverterRoute] requires the name, color, and units to not be null
   const ConverterScreen({
     @required this.name,
     @required this.color,
@@ -19,138 +28,22 @@ class ConverterScreen extends StatefulWidget {
        assert(color != null);
 
   @override
-    State<StatefulWidget> createState() => _ConverterRoute();
-
-
+    _ConverterRouteState createState() => _ConverterRouteState();
 }
 
-class _ConverterRoute extends State<ConverterScreen>
+class _ConverterRouteState extends State<ConverterScreen>
 {
-  //TODO: Set some variables, such as for keeping track of the user's input
-  // value and units
-  double _inputValue;
-  String _convertedOutput;
+
   Unit _fromValue;
   Unit _toValue;
+  double _inputValue;
+  String _convertedOutput = '';
   List<DropdownMenuItem> _unitMenuItems;
   bool _invalidInputFlag = false;
-
-  // TODO: Determine weather you need to overide anything, such as initState()
-  @override
-  void initState() { 
-    _createDropdownItems();
-    _fromValue = widget.units[0];
-    _toValue = widget.units[1];
-    super.initState();
-  }
-
-  // TODO: Add other helper functions. We've given you one, _format()
-
-  // Takes in a string from the textinput field and checks for valid input and format
-  // also updates states to show valid input in output or to deisplay error state.
-  void _updateInputValue(String input)
-  {
-    setState(() {
-          
-    if(input.isEmpty)
-    {
-      _convertedOutput = '';
-    }
-    else
-    {
-      try
-      {
-       final inputDouble = double.parse(input);
-       _inputValue = inputDouble;
-       _invalidInputFlag = false;
-        //update converted output
-      
-      } on Exception catch (e)
-      {
-        print('Error: $e');
-        _invalidInputFlag = true;   
-      }
-    }
-        });
-  }
-
-  // Clean up conversionl trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
-  String _format(double conversion)
-  {
-    var outputNum = conversion.toStringAsPrecision(7);
-    //? clearing trailing zeros
-    if(outputNum.contains('.') && outputNum.endsWith('0'))
-    {
-      var index = outputNum.length - 1;
-      while(outputNum[index] == '0')
-      {
-        index -= 1;
-      }
-      outputNum = outputNum.substring(0, index + 1);
-    }
-    if(outputNum.endsWith('.'))
-    {
-      return outputNum.substring(0, outputNum.length - 1);
-    }
-    return outputNum;
-  }
-  
-  //? creating items to go into the drop down
-  void _createDropdownItems() {
-    var items = <DropdownMenuItem>[];
-    for(var unit in widget.units)
-    {
-      items.add(DropdownMenuItem(
-        value: unit.name,
-        child: Container(
-          child: Text(
-            unit.name,
-            softWrap: true,
-          ),
-        ),
-      ));
-    }
-    setState(() {
-          _unitMenuItems = items;
-        });
-  }
-
-  Widget _createDropdownMenu() {
-    return Container(
-      margin: EdgeInsets.only(top: 16.0),
-      decoration: BoxDecoration(
-        color: Colors.cyan,
-        border: Border.all(
-          color: Colors.red[400],
-          width: 1.0,
-        ),
-      ),
-      padding: EdgeInsets.symmetric(vertical: 8.0),
-      child: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Colors.red[50],
-        ), 
-        child: DropdownButtonHideUnderline(
-          child: ButtonTheme(
-            alignedDropdown: true,
-            child: DropdownButton(
-              value: _fromValue.name,
-              style: Theme.of(context).textTheme.title,
-              items: _unitMenuItems,
-              onChanged: null,
-            ),
-          ), 
-        ),
-      ),
-    );
-  }
-
 
   @override
     Widget build(BuildContext context) 
     {
-      // TODO: Create the 'input' group of widgets. This is a Column that
-      // includes the input value, and 'from' unit [Dropdown].
     final input = Padding(
       padding: _padding,
       child: Column(
@@ -170,37 +63,206 @@ class _ConverterRoute extends State<ConverterScreen>
             onChanged: _updateInputValue,
             keyboardType: TextInputType.number,
           ),
-          _createDropdownMenu()
+          _createDropdownMenu(_fromValue.name, _updateFromConversion)
         ]
       )
     );
 
-      final arrows = RotatedBox(
-        quarterTurns: 1,
-        child: Icon(
-          Icons.compare_arrows,
-          size: 40.0,
-        ),
-      );
+    final arrows = RotatedBox(
+      quarterTurns: 1,
+      child: Icon(
+        Icons.compare_arrows,
+        size: 40.0,
+      ),
+    );
 
-      // TODO: Create the 'output' group of widgets. This is a Column that
-
-      final output = Padding(
-        padding: _padding,
-      );
-
-      final converter = Column(
+    final output = Padding(
+      padding: _padding,
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          input,
-          arrows,
-          output,
+          InputDecorator(
+            child: Text(
+              _convertedOutput,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Output',
+              labelStyle: Theme.of(context).textTheme.display1,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(0.0),
+              )
+            ),
+          ),
+          _createDropdownMenu(_toValue.name, _updateToConversion),
         ],
-      );
+      ),
+    );
 
-      return Padding(
-        padding: _padding,
-        child: converter,
-      );
+    final converter = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        input,
+        arrows,
+        output,
+      ],
+    );
+
+    return Padding(
+      padding: _padding,
+      child: converter,
+    );
+  }
+
+  @override
+  void initState() { 
+    _createDropdownItems();
+    _fromValue = widget.units[0];
+    _toValue = widget.units[1];
+    super.initState();
+  }
+  
+  /// creates [DropdownMenuItem] List of [Unit]s from wiget.units
+  /// also [setState] of [_unitMenuItems] to generated list.
+  void _createDropdownItems() {
+    var items = <DropdownMenuItem>[];
+    for(var unit in widget.units)
+    {
+      items.add(DropdownMenuItem(
+        value: unit.name,
+        child: Container(
+          child: Text(
+            unit.name,
+            softWrap: true,
+          ),
+        ),
+      ));
+    }
+    setState(() {
+          _unitMenuItems = items;
+        });
+  }
+
+  /// creates custom [DropdownButton] containing list of Units for the [Catergory]
+  /// takes in a [String] that sets the value of the Menu
+  /// takes in a [ValueChanged] that is the onchanged funciton.
+  Widget _createDropdownMenu(String givenValue, ValueChanged<dynamic> onChanged) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        border: Border.all(
+          color: Colors.grey[400],
+          width: 1.0,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[50],
+        ), 
+        child: DropdownButtonHideUnderline(
+          child: ButtonTheme(
+            alignedDropdown: true,
+            child: DropdownButton(
+              value: givenValue,
+              items: _unitMenuItems,
+              onChanged: onChanged,
+              style: Theme.of(context).textTheme.title,
+            ),
+          ), 
+        ),
+      ),
+    );
+  }
+
+  /// scrubs [_inputValue] of any trailing zeros
+  String _format(double conversion)
+  {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if(outputNum.contains('.') && outputNum.endsWith('0'))
+    {
+      var index = outputNum.length - 1;
+      while(outputNum[index] == '0')
+      {
+        index -= 1;
+      }
+      outputNum = outputNum.substring(0, index + 1);
+    }
+    if(outputNum.endsWith('.'))
+    {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
+
+  /// gets the first [Unit] that has a name that matches [unitName]
+  /// if [unitName] is not found return null
+  Unit _getUnit(String unitName) {
+    return widget.units.firstWhere(
+      (Unit unit) {
+        return unit.name == unitName;
+      },
+      orElse: null,
+    );
+  }
+  
+  ///[setState] of [_convertedOutput] to conversion based of the
+  /// [_toValue] and [_fromValue] conversion.
+  void _updateConversion(){
+    setState(() {
+          _convertedOutput = _format(_inputValue * (_toValue.conversion/ _fromValue.conversion));
+        });
+  }
+  
+  /// updates [_fromValue] to selected unit
+  /// if input value has a number, [_updateConversion] is called.
+  void _updateFromConversion(dynamic unitName) {
+    setState(() {
+          _fromValue = _getUnit(unitName);
+        });
+        if (_inputValue != null) {
+          _updateConversion();
+        }
+  }
+
+  /// checks [input] to see if it is a valid number
+  /// if it is set [_inputValue] to a double of [input] and
+  /// [_updateConversion] is called
+  /// IF IT IS NOT a valid number, [_invalidInputFlag] is set to true.
+  void _updateInputValue(String input)
+  {
+    setState(() {
+          
+    if(input.isEmpty)
+    {
+      _convertedOutput = '';
+    }
+    else
+    {
+      try
+      {
+       final inputDouble = double.parse(input);
+       _inputValue = inputDouble;
+       _invalidInputFlag = false;
+      _updateConversion();
+      } on Exception catch (e)
+      {
+        print('Error: $e');
+        _invalidInputFlag = true;   
+      }
+    }
+        });
+  }
+
+  /// updates [_toValue] to selected unit
+  /// if input value has a number, [_updateConversion] is called.
+  void _updateToConversion(dynamic unitName){
+    setState(() {
+          _toValue = _getUnit(unitName);
+        });
+        if (_inputValue != null) {
+          _updateConversion();
+        }
   }
 }
